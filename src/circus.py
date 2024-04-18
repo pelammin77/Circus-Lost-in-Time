4ec207688c4c12d0f81c2a9ff4b8f868603f6cad
Petri Lamminaho <pelammin77@gmail.com>
2024-04-18 04:17:22 +0300

    Images to project

--- src/circus.py
+++ src/circus.py
@@ -1,6 +1,58 @@
 import pygame
-
+import random
+from colors import colors
+from Characters import *
 pygame.init()
+
+
+"""
+import pygame
+
+class Character(pygame.sprite.Sprite):
+    def __init__(self, image_path, position, speed):
+        super().__init__()
+        self.image = pygame.image.load(image_path)
+        self.rect = self.image.get_rect(topleft=position)
+        self.speed = speed
+
+    def move(self, direction):
+        if direction == 'up':
+            self.rect.y -= self.speed
+        elif direction == 'down':
+            self.rect.y += self.speed
+        elif direction == 'left':
+            self.rect.x -= self.speed
+        elif direction == 'right':
+            self.rect.x += self.speed
+
+    def update(self):
+        # Tähän voi lisätä päivityslogiikkaa, kuten törmäysten tarkistuksen
+        pass
+
+
+
+class Acrobat(Character):
+    def jump(self):
+        # Toteuta hyppytoiminto
+        print("Jumping!")
+
+class KnifeThrower(Character):
+    def throw_knife(self):
+        # Toteuta veitsenheitto
+        print("Throwing a knife!")
+
+class Strongman(Character):
+    def lift(self):
+        # Toteuta nostotoiminto
+        print("Lifting!")
+"""
+
+
+
+
+
+
+
 
 WIDTH = 800
 HEIGTH = 600
@@ -15,7 +67,7 @@ font = pygame.font.SysFont('Arial', 24)
 run = True
 while run:
     timer.tick(fps)
-    window.fill("blue")
+    window.fill(colors["sky_blue"])
     for event in pygame.event.get():
        if event.type == pygame.QUIT:
            run = False
---
+++ src/colors.py
@@ -0,0 +1,19 @@
+
+# colors
+colors = {
+    'light_blue': (173, 216, 230),
+    'sky_blue': (135, 206, 235),
+    'grass_green': (124, 252, 0),  # Kevyt, kirkas ruohon vihreä
+    'deep_green': (0, 100, 0),     # Syvä tummanvihreä
+    'red': (255, 0, 0),            # Kirkas punainen
+    'dark_red': (139, 0, 0),       # Tumma punainen
+    'blue': (0, 0, 255),           # Kirkas sininen
+    'navy_blue': (0, 0, 128),      # Tumma laivastonsininen
+    'yellow': (255, 255, 0),       # Kirkas keltainen
+    'orange': (255, 165, 0),       # Oranssi
+    'white': (255, 255, 255),      # Valkoinen
+    'black': (0, 0, 0),            # Musta
+    'grey': (128, 128, 128),       # Harmaa
+    'purple': (128, 0, 128),       # Violetti
+    'brown': (165, 42, 42)         # Ruskea
+}
