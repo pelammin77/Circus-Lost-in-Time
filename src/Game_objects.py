a28a469a2e25b12bfb1d89d487851bbc827c1b2d
Petri Lamminaho <pelammin77@gmail.com>
2024-04-23 02:26:27 +0300

    Game_object class

---
+++ src/Game_objects.py
@@ -0,0 +1,24 @@
+import pygame
+
+class Floor:
+    pass
+
+class WoodFloor(Floor):
+    pass
+
+class SteelFloor(Floor):
+    pass
+
+class Ground:
+    pass
+
+class GrassGround(Ground):
+    pass
+
+class Wall:
+    pass
+
+
+
+class Leader:
+    pass
